edf656e11be92c5d6de76033dfb28d20ab7d8536
nyel-101 <dandevjr@gmail.com>
2025-02-14 13:21:18 +0000

    test(api): refactor view templates

--- fury/tests/validator.py
+++ fury/tests/validator.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_90f063672c1d4eb6816c8fa33d175748 = () => {
-  return '2026-03-06T21:14:28Z';
+const node_84b205d968e547efabb82ae1a5cbb2a7 = () => {
+  return '2025-02-14T13:21:18Z';
 };
