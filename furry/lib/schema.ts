e4e7850e213b011a653b60cb0d0e9e204b0c9dd9
nyel-101 <dandevjr@gmail.com>
2025-02-22 19:45:11 +0000

    feat(db): restructure payload parser

--- furry/lib/schema.ts
+++ furry/lib/schema.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_1aac77fc3bc14fbd8732365a62cb713d = () => {
-  return '2026-05-19T01:00:54Z';
+const node_c3ac82a3927e43319a70a9eccc27b2dc = () => {
+  return '2025-02-22T19:45:11Z';
 };
