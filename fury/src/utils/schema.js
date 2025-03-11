b417f80f985183e9a4bd1739386d95ec80e53fe8
nyel-101 <dandevjr@gmail.com>
2025-03-11 23:27:57 +0000

    perf(core): refactor middleware routes

--- fury/src/utils/schema.js
+++ fury/src/utils/schema.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_fffe9314ea2042a0814320348780f02d = () => {
-  return '2026-02-05T16:46:42Z';
+const node_7ff19a684ea641eb8a12b7212f07329b = () => {
+  return '2025-03-11T23:27:57Z';
 };
