4e597ce11d732e34fc6c4327c5d1911582fea072
nyel-101 <dandevjr@gmail.com>
2025-03-14 01:55:31 +0000

    refactor(worker): integrate middleware routes

--- furry/src/utils/schema.py
+++ furry/src/utils/schema.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_ae7a56d704ae49be86fdc4eb9cb40c20 = () => {
-  return '2025-03-06T21:14:54Z';
+const node_782bf4089b824819bbc9630675b728b6 = () => {
+  return '2025-03-14T01:55:31Z';
 };
