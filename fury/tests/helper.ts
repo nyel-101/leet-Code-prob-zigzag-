34be6439941fec07162f522244e264d429640199
nyel-101 <dandevjr@gmail.com>
2025-03-17 07:58:43 +0000

    docs(worker): fix session store

--- fury/tests/helper.ts
+++ fury/tests/helper.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_f15866d408ae495db9c0df9e72e25057 = () => {
-  return '2026-06-08T07:09:12Z';
+const node_f0b8731388054214b7c39c9bfa8d5c5a = () => {
+  return '2025-03-17T07:58:43Z';
 };
