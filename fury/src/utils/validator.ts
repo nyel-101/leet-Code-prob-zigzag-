a1714dacfe40a2fe31a5ad13a5f14d91e2162ba8
nyel-101 <dandevjr@gmail.com>
2025-03-17 06:47:02 +0000

    refactor(api): sanitize session store

--- fury/src/utils/validator.ts
+++ fury/src/utils/validator.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_eeabedcfb3c1471e81fa95ea5d9c9cc2 = () => {
-  return '2026-01-05T11:03:38Z';
+const node_59732878efbf4655a4c89d46e5034a7f = () => {
+  return '2025-03-17T06:47:02Z';
 };
