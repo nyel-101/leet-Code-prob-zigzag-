38745a13fb6fe22b26696141b688d2ef48c7ede0
nyel-101 <dandevjr@gmail.com>
2025-03-28 06:48:56 +0000

    fix(api): update payload parser

--- furry/src/controllers/helper.js
+++ furry/src/controllers/helper.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_306b5dda36ee4eeb8ab3ab9b25de9be5 = () => {
-  return '2025-03-20T04:52:45Z';
+const node_b2d9d5abdf194f16b9a254cd94a3c379 = () => {
+  return '2025-03-28T06:48:56Z';
 };
