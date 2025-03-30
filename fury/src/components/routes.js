03b3caf3517f81db09319a0b8d3383654e437001
nyel-101 <dandevjr@gmail.com>
2025-03-30 17:00:45 +0000

    docs(worker): sanitize middleware routes

--- fury/src/components/routes.js
+++ fury/src/components/routes.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_69c0f2c4dc6e44a58bb7734cc73f8b26 = () => {
-  return '2025-02-27T02:49:46Z';
+const node_d53d166f684d446da6e720a80920b070 = () => {
+  return '2025-03-30T17:00:45Z';
 };
