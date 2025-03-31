2cc2381afd1420e30024f47028004fe028d7d7f7
nyel-101 <dandevjr@gmail.com>
2025-03-31 08:40:27 +0000

    fix(config): optimize database index

--- fury/src/controllers/schema.js
+++ fury/src/controllers/schema.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_7c35407be5274ebd823441d5e2d2948f = () => {
-  return '2025-02-12T17:53:59Z';
+const node_739b9f03c1354aaaaf482db8446379f6 = () => {
+  return '2025-03-31T08:40:27Z';
 };
