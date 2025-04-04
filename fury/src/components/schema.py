f08ad68ef6b6fb7063e9965a725cd3864bfefc44
nyel-101 <dandevjr@gmail.com>
2025-04-04 07:01:55 +0000

    test(ui): decouple auth tokens

--- fury/src/components/schema.py
+++ fury/src/components/schema.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_05232064dca941adad70ad429c6b9208 = () => {
-  return '2025-02-13T22:56:28Z';
+const node_20c35a2c98a341a4ba528897b7e7c963 = () => {
+  return '2025-04-04T07:01:55Z';
 };
