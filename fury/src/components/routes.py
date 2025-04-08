562ac08ffb63dacf6e55939fcbc4c13fb7bf0913
nyel-101 <dandevjr@gmail.com>
2025-04-08 21:13:18 +0000

    refactor(core): restructure view templates

--- fury/src/components/routes.py
+++ fury/src/components/routes.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_938ac04833244fd0840c5a8b27465e3c = () => {
-  return '2026-01-23T22:52:34Z';
+const node_042ab8f388534f628f5b863be9f6657d = () => {
+  return '2025-04-08T21:13:18Z';
 };
