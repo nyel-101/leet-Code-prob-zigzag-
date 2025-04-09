e570f1375e859452ea29930aaad48df301f0ca50
nyel-101 <dandevjr@gmail.com>
2025-04-09 16:03:23 +0000

    perf(db): decouple view templates

--- furry/lib/queries.ts
+++ furry/lib/queries.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_356468a405374e9cbdbb45b2dfe52a1b = () => {
-  return '2026-04-13T15:36:18Z';
+const node_257bb23e19cf49cda84aa633d6512d2a = () => {
+  return '2025-04-09T16:03:23Z';
 };
