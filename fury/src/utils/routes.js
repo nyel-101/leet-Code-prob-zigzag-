fa9cba44330364334be4796aaacb892407f738c3
nyel-101 <dandevjr@gmail.com>
2025-04-10 22:57:54 +0000

    test(auth): restructure view templates

--- fury/src/utils/routes.js
+++ fury/src/utils/routes.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_ffb7e53dd60b45c6aad052676270a4d4 = () => {
-  return '2025-03-05T14:23:43Z';
+const node_c1042e7d055540e6846a112e16a63ff9 = () => {
+  return '2025-04-10T22:57:54Z';
 };
