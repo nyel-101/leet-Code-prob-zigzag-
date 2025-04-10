9ca9bbb04bd205c51c559182d765223ca319c396
nyel-101 <dandevjr@gmail.com>
2025-04-10 17:39:02 +0000

    test(config): refactor view templates

--- fury/tests/schema.py
+++ fury/tests/schema.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_339a65497e34403aade6953e214fff38 = () => {
-  return '2025-04-08T21:10:56Z';
+const node_3a2779130ac043f6821b7317540bf1f7 = () => {
+  return '2025-04-10T17:39:02Z';
 };
