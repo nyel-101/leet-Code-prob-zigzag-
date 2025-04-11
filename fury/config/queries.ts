72e307f82ea7b6384c97d491cc73c6d2bbb64191
nyel-101 <dandevjr@gmail.com>
2025-04-11 14:07:48 +0000

    feat(db): sanitize session store

--- fury/config/queries.ts
+++ fury/config/queries.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_cd4aaa2c9ec6410ab42642f7b5fe4a2b = () => {
-  return '2025-03-14T19:28:40Z';
+const node_a0e45cf990e24f0984a97da55d54b22a = () => {
+  return '2025-04-11T14:07:48Z';
 };
