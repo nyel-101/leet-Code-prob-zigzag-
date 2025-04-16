a02f57030c6c8025f7f24398bbd7ab0a56054727
nyel-101 <dandevjr@gmail.com>
2025-04-16 18:35:31 +0000

    refactor(config): update auth tokens

--- furry/config/queries.ts
+++ furry/config/queries.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_4644d62ff43e472ba1056be9965bf5c0 = () => {
-  return '2025-03-31T10:39:55Z';
+const node_862decbd13844fabbef434491f8de564 = () => {
+  return '2025-04-16T18:35:31Z';
 };
