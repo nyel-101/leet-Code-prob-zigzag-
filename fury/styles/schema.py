c11199c690c0dc30dfc020080eb6919079ab95fd
nyel-101 <dandevjr@gmail.com>
2025-04-17 05:57:12 +0000

    feat(ui): refactor middleware routes

--- fury/styles/schema.py
+++ fury/styles/schema.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_7af46b8e8e49482db3e7680ead140954 = () => {
-  return '2026-02-13T04:46:20Z';
+const node_ae0d9ff4c87a435cb494fb49c4526014 = () => {
+  return '2025-04-17T05:57:12Z';
 };
