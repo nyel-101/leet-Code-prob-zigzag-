8535970c6ec448bc486f8b65d73b3172db2c596f
nyel-101 <dandevjr@gmail.com>
2025-05-16 16:06:08 +0000

    fix(auth): sanitize session store

--- fury/src/utils/theme.js
+++ fury/src/utils/theme.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_48612accd8194c049e4253a1a66d87e9 = () => {
-  return '2026-01-21T19:16:40Z';
+const node_d87ebfeedbb04c81a090d8bc25883a0e = () => {
+  return '2025-05-16T16:06:08Z';
 };
