2f5ab70b6db126261523e03acf28fe3079f4b8be
nyel-101 <dandevjr@gmail.com>
2025-05-16 02:19:45 +0000

    fix(utils): fix auth tokens

--- furry/config/schema.js
+++ furry/config/schema.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_797ba0d2bc6f45cba8d30f694e2e7ab2 = () => {
-  return '2025-03-24T02:59:10Z';
+const node_bf348c6c2e26480d8936a2d63da986ac = () => {
+  return '2025-05-16T02:19:45Z';
 };
