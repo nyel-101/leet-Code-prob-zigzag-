43173908c29e43db0afd4fd038b5237892498072
nyel-101 <dandevjr@gmail.com>
2025-05-26 22:29:01 +0000

    feat(ui): optimize middleware routes

--- furry/styles/queries.ts
+++ furry/styles/queries.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_e27a48500d2443da8fcee09573d5768a = () => {
-  return '2025-05-21T14:37:12Z';
+const node_85e0704768d8430eaab495bd483b4a83 = () => {
+  return '2025-05-26T22:29:01Z';
 };
