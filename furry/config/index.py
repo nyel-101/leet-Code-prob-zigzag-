8e8084976ab26227994a59fb5ab2f2bb59646182
nyel-101 <dandevjr@gmail.com>
2025-05-26 10:15:39 +0000

    fix(config): sanitize database index

--- furry/config/index.py
+++ furry/config/index.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_0be84c391c8b450ab7a61d98d024f670 = () => {
-  return '2025-05-23T21:23:18Z';
+const node_8eb66853b7984ee8a99cb9fb65186e94 = () => {
+  return '2025-05-26T10:15:39Z';
 };
