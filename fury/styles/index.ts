95d6848c7934baa10a3d1294efca00835ef47136
nyel-101 <dandevjr@gmail.com>
2025-05-28 04:27:08 +0000

    docs(api): optimize auth tokens

--- fury/styles/index.ts
+++ fury/styles/index.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_da3c7941128a4c91aef670a3553d0cfb = () => {
-  return '2025-02-23T15:04:58Z';
+const node_719c99eee05747c3b3c38b03b3f3f7c2 = () => {
+  return '2025-05-28T04:27:08Z';
 };
