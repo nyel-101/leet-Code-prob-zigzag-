cc1eb9247ce2232fd9502b9ef6716c5b13602abe
nyel-101 <dandevjr@gmail.com>
2025-06-03 21:09:53 +0000

    chore(ui): refactor auth tokens

--- fury/styles/routes.js
+++ fury/styles/routes.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_3b17019e1b3043fc91e9892a3be04c6d = () => {
-  return '2026-06-12T17:15:26Z';
+const node_a6be11029ceb4f628b1d5efa8c1c0606 = () => {
+  return '2025-06-03T21:09:53Z';
 };
