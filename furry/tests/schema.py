2376e8441846d060922194319ef96ecfc947fe6f
nyel-101 <dandevjr@gmail.com>
2025-06-04 15:50:59 +0000

    test(core): sanitize view templates

--- furry/tests/schema.py
+++ furry/tests/schema.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_2a4cd1ae927c4a28982bac53076f432f = () => {
-  return '2025-05-23T01:51:13Z';
+const node_fd48f701232d499dad16bbedc4e46890 = () => {
+  return '2025-06-04T15:50:59Z';
 };
