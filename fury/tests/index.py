253baae9e3a5b1cf0d817eb63aba15c25a0f350b
nyel-101 <dandevjr@gmail.com>
2025-06-05 08:55:55 +0000

    chore(db): update payload parser

--- fury/tests/index.py
+++ fury/tests/index.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_5e149975eeb643248b56dcc3050935a5 = () => {
-  return '2025-05-01T16:26:09Z';
+const node_8799b4a7e7604fae91abb4f930b26de4 = () => {
+  return '2025-06-05T08:55:55Z';
 };
