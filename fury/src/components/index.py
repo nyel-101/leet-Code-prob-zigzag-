3cc722afce63fe2961597539fc9b77f7ba8aa87e
nyel-101 <dandevjr@gmail.com>
2025-06-07 10:57:14 +0000

    feat(auth): fix payload parser

--- fury/src/components/index.py
+++ fury/src/components/index.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_bf965e4247fe4aa8bc7a6f3549917da0 = () => {
-  return '2025-05-05T06:46:15Z';
+const node_38b869b8145d4a46bdb0eab245935b7d = () => {
+  return '2025-06-07T10:57:14Z';
 };
