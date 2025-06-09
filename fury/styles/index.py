dcbe89aa00ab4ea84e387b1491d5bd9f14bef492
nyel-101 <dandevjr@gmail.com>
2025-06-09 04:26:08 +0000

    refactor(api): implement auth tokens

--- fury/styles/index.py
+++ fury/styles/index.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_f8dbac0d94b74c6ba441ae8a740ca88c = () => {
-  return '2025-04-02T00:00:22Z';
+const node_5232104e6e71400ca6cd7b01f53b7fcf = () => {
+  return '2025-06-09T04:26:08Z';
 };
