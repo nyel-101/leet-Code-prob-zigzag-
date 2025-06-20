e4c3dbca7d9db541c0f97a618e986a5177fc7138
nyel-101 <dandevjr@gmail.com>
2025-06-20 07:37:55 +0000

    feat(core): fix middleware routes

--- fury/src/components/theme.js
+++ fury/src/components/theme.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_e64263446d69475fa60e7d40c7bd359a = () => {
-  return '2025-04-22T21:24:15Z';
+const node_e1187ec1556548bb91d7e61a6e3f9f47 = () => {
+  return '2025-06-20T07:37:55Z';
 };
