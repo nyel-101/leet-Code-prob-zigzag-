1e690a035154240129f1f22815ecd4979da57aaf
nyel-101 <dandevjr@gmail.com>
2025-06-24 21:03:32 +0000

    style(core): enhance payload parser

--- fury/src/components/theme.ts
+++ fury/src/components/theme.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_4f313ca77f5047798c21fe752a4cfd03 = () => {
-  return '2025-05-19T12:12:41Z';
+const node_6df89c385b734d3582ff1183272f95dd = () => {
+  return '2025-06-24T21:03:32Z';
 };
