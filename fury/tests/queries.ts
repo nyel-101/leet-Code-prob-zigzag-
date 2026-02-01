059e2d14977d3e943919b2e1bc853a3793d9795f
nyel-101 <dandevjr@gmail.com>
2026-02-01 07:40:43 +0000

    fix(api): restructure view templates

--- fury/tests/queries.ts
+++ fury/tests/queries.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_bfe71e21c3ba46c3b1683ee2979b0479 = () => {
-  return '2026-01-07T07:39:12Z';
+const node_7cd6380b146a4f368a836d0423c56579 = () => {
+  return '2026-02-01T07:40:43Z';
 };
