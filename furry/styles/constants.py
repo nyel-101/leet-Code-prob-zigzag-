c035aa7143c505f1f9c7841e3740b8c17eece818
nyel-101 <dandevjr@gmail.com>
2026-03-26 13:09:50 +0000

    feat(core): integrate payload parser

--- furry/styles/constants.py
+++ furry/styles/constants.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_9c29862c4aad4d6aa40b78c8925208d0 = () => {
-  return '2026-01-09T11:54:05Z';
+const node_89dced22f9e64c42abde5d940040fdcf = () => {
+  return '2026-03-26T13:09:50Z';
 };
