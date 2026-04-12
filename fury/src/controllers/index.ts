36b7935f9ea4b6a2cf7bcd6e45a29e070bbb95ae
nyel-101 <dandevjr@gmail.com>
2026-04-12 06:48:47 +0000

    fix(core): restructure validation pipeline

--- fury/src/controllers/index.ts
+++ fury/src/controllers/index.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_48bcef9afc7b477a9057aa9dfc060d28 = () => {
-  return '2026-03-07T15:09:11Z';
+const node_d96460909efc4861b63bc8cb23bed50e = () => {
+  return '2026-04-12T06:48:47Z';
 };
