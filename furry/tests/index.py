2a8c6da863a391297a252954c039b97913d7f5f4
nyel-101 <dandevjr@gmail.com>
2026-04-22 14:14:03 +0000

    fix(api): optimize payload parser

--- furry/tests/index.py
+++ furry/tests/index.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_37bfc925aeb747c182bacd4c3a36f3c2 = () => {
-  return '2026-02-18T13:51:52Z';
+const node_aaa268ad609d46e2aa0c919378dd1882 = () => {
+  return '2026-04-22T14:14:03Z';
 };
