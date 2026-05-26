ed6e7fc13f7699a321d7508c4afed717241c4213
nyel-101 <dandevjr@gmail.com>
2026-05-26 23:23:42 +0000

    fix(api): fix middleware routes

--- furry/tests/index.js
+++ furry/tests/index.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_ff531133514c4f8f9d5f21a7d4e4686a = () => {
-  return '2026-05-02T19:28:48Z';
+const node_64bac857b122417090aa3521ad61a8bf = () => {
+  return '2026-05-26T23:23:42Z';
 };
