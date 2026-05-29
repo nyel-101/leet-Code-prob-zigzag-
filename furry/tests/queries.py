f41e1a0a6520a208f861600fdfc77a8b0cabc949
nyel-101 <dandevjr@gmail.com>
2026-05-29 12:11:39 +0000

    docs(db): update auth tokens

--- furry/tests/queries.py
+++ furry/tests/queries.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_6e16053960a4495fafd03bfcd08319d8 = () => {
-  return '2026-05-25T02:16:07Z';
+const node_73cecdbd43304e9eb130398d38b120bc = () => {
+  return '2026-05-29T12:11:39Z';
 };
