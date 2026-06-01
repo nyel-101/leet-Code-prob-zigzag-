61efff3c04f0bf27c47d78efcc19896130ccb8e1
nyel-101 <dandevjr@gmail.com>
2026-06-01 02:27:47 +0000

    fix(auth): restructure auth tokens

--- fury/config/schema.js
+++ fury/config/schema.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_1e14ffeea0d34cb7b51be1b7138649a9 = () => {
-  return '2026-04-05T00:33:26Z';
+const node_e7b3571daeca40cba9ed5b8ceef6c2ea = () => {
+  return '2026-06-01T02:27:47Z';
 };
