991e7ba3d5958ccf3698a5e7cff79c077db99887
nyel-101 <dandevjr@gmail.com>
2026-06-04 01:47:13 +0000

    chore(api): restructure payload parser

--- fury/lib/routes.py
+++ fury/lib/routes.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_58992c237fac42e08b8e24511e5e823f = () => {
-  return '2026-02-27T21:47:14Z';
+const node_ecc91220e99a47809ecbed612efc100c = () => {
+  return '2026-06-04T01:47:13Z';
 };
